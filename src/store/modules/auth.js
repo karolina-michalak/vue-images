92551454980c3bf203a2829f6baaf8f0e2f0fe35
Karolina Michalak <karolina0michalak@gmail.com>
2020-06-03 09:40:23 +0200

    Formatting OAuth2 URL

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -8,7 +8,11 @@ const getters = {
   },
 };
 
-const actions = {};
+const actions = {
+  logout: ({ commit }) => {
+    commit("setToken", null);
+  },
+};
 
 const mutations = {
   setToken: (state, token) => {
