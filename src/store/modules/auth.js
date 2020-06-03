6dccd15af540a26cb3353e4c518b87eaa716f3a2
Karolina Michalak <karolina0michalak@gmail.com>
2020-06-03 09:50:39 +0200

    Login function

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import api from "../../api/imgur";
+
 const state = {
   token: null,
 };
@@ -9,6 +11,9 @@ const getters = {
 };
 
 const actions = {
+  login: () => {
+    api.login();
+  },
   logout: ({ commit }) => {
     commit("setToken", null);
   },
